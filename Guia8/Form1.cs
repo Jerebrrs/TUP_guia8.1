d4f378bc95ebb067c9d9e706435617042f7294b0
Kevin Jeremias Barrios <kevinbrrs96@gmail.com>
2025-10-02 17:39:13 -0300

    actualizacion ejercicio en clase

---
+++ Guia8/Form1.cs
@@ -0,0 +1,93 @@
+﻿using Guia8.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guia8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<Cuenta> listaCuenta = new List<Cuenta>();
+        int v=-1;
+        private void btnConfitmar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                
+                string nombre = tbNombre.Text;
+                int dni = Convert.ToInt32(tbDni.Text);
+                double imp = Convert.ToDouble(tbImporte.Text);
+
+                
+                if (nombre != "" && dni != null && imp != null)
+                {
+                 
+
+                    if (v == -1)
+                    {
+                        Cuenta cuenta = new Cuenta(nombre, dni, imp);
+                        listaCuenta.Add(cuenta);
+                        MessageBox.Show($@"Sasfgwregerggee :  {cuenta.Nombre}");
+                    }
+                    else
+                    {
+
+                        Cuenta cuenta = new Cuenta(nombre, dni, imp);
+
+                        listaCuenta[v] = cuenta;
+
+
+                        MessageBox.Show($@"oisghasoighaoishgoiashigoisohasoihgoiasg{imp}");
+                        v = -1;
+                    }
+
+                  
+                }
+               
+                tbDni.Clear();
+                tbImporte.Clear();
+                tbNombre.Clear();
+               
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Los campos no pueden estar vacios");
+            }
+          
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            lbActualizar.Items.Clear();
+            foreach (Cuenta c in listaCuenta)
+            {
+                lbActualizar.Items.Add(c.ToString());
+            }
+        }
+
+        private void lbActualizar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            v = lbActualizar.SelectedIndex;
+            Cuenta cuenta = listaCuenta[v];
+            tbNombre.Text = cuenta.Nombre;
+            tbDni.Text = Convert.ToString(cuenta.Dni);
+            tbImporte.Text = Convert.ToString(cuenta.Importe);
+
+
+
+        }
+    }
+}
---
+++ Guia8/Models/Cuenta.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace Guia8.Models
+{
+    public class Cuenta : IComparable<Cuenta>
+    {
+        public string Nombre{ get; set; }
+        public int Dni { get; set; }
+        public double Importe { get; set; }
+
+        public Cuenta() { }
+        public Cuenta(string nombre,int dni,double importe)
+        {
+            this.Nombre= nombre; 
+            this.Dni= dni;
+            this.Importe= importe;
+        }
+
+        public string ToString()
+        {
+            return $@"Nombre: {Nombre} - Dni :{Dni} - Importe: {Importe}";
+        }
+
+        public int CompareTo(Cuenta other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
